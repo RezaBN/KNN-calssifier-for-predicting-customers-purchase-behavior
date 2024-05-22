8ea81cbf497b0628feaf4573a892c84f9bd825d9
Reza Barzegar Nozari <49522160+RezaBN@users.noreply.github.com>
2024-05-23 01:39:48 +0330

    Update kdtree_knn.py
    Update kdtree_knn.py

--- KDTree kNN/kdtree_knn.py
+++ KDTree kNN/kdtree_knn.py
@@ -5,11 +5,10 @@ This script implements a simple K-Nearest Neighbors classifier for predicting on
 It is based on the programming exercise in "Shopping - CS50's Introduction to Artificial Intelligence with Python" course.
 https://cs50.harvard.edu/college/2024/fall/
 
-Author: Reza Barzegar Nozari;
-
 Description:
 - KDTree implementation uses a KDTree for efficient nearest neighbor searches.
 
+Author: Reza Barzegar Nozari
 Date: May 2024
 """
 
